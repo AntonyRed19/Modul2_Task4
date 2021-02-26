23d2ef62560584acf314f12f071c0fde6eebc597
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-26 13:51:59 +0200

    added class KindofAnimal

---
+++ SafariPark/SafariPark/Hierarchy/KindofAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafariPark.Hierarchy.Enums;
+
+namespace SafariPark.Hierarchy
+{
+    public abstract class KindofAnimal
+    {
+        public abstract Kind Kind { get; set; }
+    }
+}
