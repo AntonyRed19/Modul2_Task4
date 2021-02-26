10ca24e64788b4316e2a22d4576e186e81cf4c90
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-26 13:38:22 +0200

    added class DetachmentofAnimal

---
+++ SafariPark/SafariPark/Hierarchy/DetachmentofAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafariPark.Hierarchy.Enums;
+
+namespace SafariPark.Hierarchy
+{
+    public abstract class DetachmentofAnimal : ClassofAnimal
+    {
+        public Detachment Detachment { get; set; }
+    }
+}
