eea75dda8a536b3052ed2956e4e728395a94290b
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-26 13:22:47 +0200

    added Class Animal

---
+++ SafariPark/SafariPark/Hierarchy/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafariPark.Hierarchy.Enums;
+
+namespace SafariPark.Hierarchy
+{
+    public abstract class Animal
+    {
+        public abstract double Weigh { get; set; }
+        public abstract double Growth { get; set; }
+        public abstract string Name { get; set; }
+        public abstract Country HomeCountry { get; set; }
+
+        public abstract DateTime DateArrival { get; set; }
+
+        public abstract TimeSpan LifeTime { get; set; }
+        public abstract int Age { get; set; }
+    }
+}
