792842ffacc104cf8c59892b6c8693935f6eedd3
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-26 19:17:04 +0200

    added Extention and Starter

---
+++ SafariPark/SafariPark/Starter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafariPark.Helpers;
+using SafariPark.Services;
+using SafariPark.Services.Abstractions;
+using SafariPark.Hierarchy;
+
+namespace SafariPark
+{
+    public class Starter
+    {
+        private readonly ISafaryServices _safariService;
+
+        public Starter()
+        {
+            _safariService = new SafariServices();
+        }
+
+        public void Run()
+        {
+            Animal();
+        }
+
+        private void Animal()
+        {
+            var animal = _safariService.MakeSection();
+            Array.Sort(animal.KindofAnimals, new AnimalComparer());
+
+            for (int i = 0; i < animal.KindofAnimals.Length; i++)
+            {
+                Console.WriteLine($"Name: {animal.KindofAnimals[i].Name} Age: {animal.KindofAnimals[i].Age}");
+            }
+
+            Console.WriteLine($"Count of Animals: {animal.Count}");
+
+            var animals = animal.FindAnimal(10);
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine($"Name: {animals[i].Name} Kind: {animals[i].Kind} Age: {animals[i].Age}");
+            }
+        }
+    }
+}
